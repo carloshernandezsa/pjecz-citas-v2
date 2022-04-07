a8acb6c66d3370a6f3b1b5d0dd4db9540e1920bc
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2022-04-07 12:41:31 -0500

    Prueba con cambio en main

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 
 
 # Decidir entre arrancar el sistema para el cliente o para administracion
-if os.environ.get("FLASK_APP", "citas_admin/app.py") == "citas_admin/app.py":
+if os.environ.get("FLASK_APP", "citas_admin.app") == "citas_admin.app":
     from citas_admin import app
 else:
     from citas_cliente import app
